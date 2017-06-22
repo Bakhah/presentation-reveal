7833be52cafc4b311410eb3415b55f0b8dabfda2
Franck Hourdin <franck.hourdin@pictotravel.fr>
2017-06-22 10:39:25 +0200

    Morgan

--- web.js
+++ web.js
@@ -4,6 +4,9 @@ var app = express();
 app.use(express.logger());
 app.use("/", express.static(__dirname));
 
+var logger = require('morgan');
+app.use(logger);
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
