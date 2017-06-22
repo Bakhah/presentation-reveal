1253b77f64c20a20c3056d3058ef625e75e0f677
Franck Hourdin <franck.hourdin@pictotravel.fr>
2017-06-22 10:41:28 +0200

    morgan gain

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 
 var express = require("express");
 var app = express();
-app.use(express.logger());
-app.use("/", express.static(__dirname));
-
 var logger = require('morgan');
+
 app.use(logger);
+app.use("/", express.static(__dirname));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
