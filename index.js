0575f6ad1b34a4f798b831df93b288803fe38530
bakhah <franck.hourdin@pictotravel.fr>
2017-06-22 10:22:51 +0200

    Fix

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+app.use(express.logger());
+app.use("/", express.static(__dirname));
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('index');
-});
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
