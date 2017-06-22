89658146cba567a45022223d0bee80fdf9b61223
Franck Hourdin <franck.hourdin@pictotravel.fr>
2017-06-22 10:24:44 +0200

    Bisous

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 app.use(express.logger());
 app.use("/", express.static(__dirname));
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
